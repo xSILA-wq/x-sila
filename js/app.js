d80880aed95e3c3a8f51cfa3609c348e0534005b
xSILA-wq <nerminmuhic638@gmail.com>
2024-07-30 23:58:35 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('rosterButton').onclick = function() {
+    window.location.href = './pages/roster.html';
+};
+document.getElementById('contactButton').onclick = function() {
+    window.location.href = './pages/owners.html';
+};
+document.getElementById('scrimResults').onclick = function() {
+    window.location.href = './pages/scrims.html';
+};
+
